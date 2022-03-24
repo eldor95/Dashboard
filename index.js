e124f4813e08d8a52d9101e6bdbd6253fcb70981
eldor <eldorodilov2@gmail.com>
2022-03-25 02:51:45 +0500

    add
    addd

--- index.js
+++ index.js
@@ -21,14 +21,21 @@ themeToggle.addEventListener("click", () => {
   themeToggle.querySelector("span:nth-child(1)").classList.toggle("active");
   themeToggle.querySelector("span:nth-child(2)").classList.toggle("active");
 });
-
+// console.log(Orders, "sdsa");
 //  fill orders in table
 Orders.forEach((order) => {
   const tr = document.createElement("tr");
-  const trContent = `<td>${order.productName}</td>
+  const trContent = `
+          <td>${order.productName}</td>
           <td>${order.productNumber}</td>
           <td>${order.paymentStatus}</td>
-          <td class="warning">${order.shipping}</td>
+          <td class="${
+            order.shipping === "Declined"
+              ? "danger"
+              : order.shipping === "pending"
+              ? "warning"
+              : "primary"
+          }">${order.shipping}</td>
           <td class="primary">Details</td>`;
 
   tr.innerHTML = trContent;
